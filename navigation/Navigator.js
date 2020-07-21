791c06d39491f804d73ac78c69dc89baa71663ca
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-07-21 15:02:32 -0400

    side drawer + icons + orders screen

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '../constants/Colors';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,7 +9,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import ProductsOverviewScreen from '../screens/ProductsOverviewScreen';
 import CartScreen from '../screens/CartScreen';
 import ProductDetailScreen from '../screens/ProductDetailScreen';
-
+import OrdersScreen from '../screens/OrdersScreen';
 
 import HeaderButton from '../components/UI/HeaderButton';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
@@ -24,6 +26,17 @@ const HomeStack = ({ navigation }) => {
     return (
         <Stack.Navigator>
             <Stack.Screen name="AllProducts" component={ProductsOverviewScreen} options={{
+                    headerLeft: () => (
+                        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                            <Item
+                                title="Menu"
+                                iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+                                onPress={() => {
+                                    navigation.toggleDrawer();
+                                }}
+                            />
+                        </HeaderButtons>
+                      ),
                     headerRight: () => (
                         <HeaderButtons HeaderButtonComponent={HeaderButton}>
                             <Item
@@ -42,6 +55,7 @@ const HomeStack = ({ navigation }) => {
             }}
             />
             <Stack.Screen name="CartScreen" component={CartScreen} options={{ title: 'Cart' }} />
+            <Stack.Screen name="OrdersScreen" component={OrdersScreen} options={{ title: 'Orders' }} />
         </Stack.Navigator>
     );
 };
@@ -50,8 +64,33 @@ const Navigation = ( ) => {
     return (
         <NavigationContainer>
             <Drawer.Navigator initialRouteName="ProductsScreen">
-                <Drawer.Screen name="ProductsScreen" component={HomeStack} />
-                <Drawer.Screen name="CartScreen" component={CartScreen} />
+                <Drawer.Screen name="ProductsScreen" component={HomeStack} options={{
+                    drawerIcon: drawerConfig => (
+                        <Ionicons
+                          name={Platform.OS === 'android' ? 'md-clipboard' : 'ios-clipboard'}
+                          size={23}
+                          color={drawerConfig.tintColor}
+                        />
+                      ),
+                }}/>
+                <Drawer.Screen name="CartScreen" component={CartScreen} options={{
+                    drawerIcon: drawerConfig => (
+                        <Ionicons
+                          name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+                          size={23}
+                          color={drawerConfig.tintColor}
+                        />
+                      ),
+                }}/>
+                <Stack.Screen name="OrdersScreen" component={OrdersScreen} options={{ title: 'Orders' }} options={{
+                    drawerIcon: drawerConfig => (
+                        <Ionicons
+                          name={Platform.OS === 'android' ? 'md-body' : 'ios-body'}
+                          size={23}
+                          color={drawerConfig.tintColor}
+                        />
+                      ),
+                }}/>
             </Drawer.Navigator>
         </NavigationContainer>
     );
